e8c134c9f023ee05020d292a504ce23430ddc1fc
mulagura <mulagura@gmail.com>
2019-11-23 10:37:16 +0530

    forms update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
+
+  public minDate = new Date(); // update later
+  public maxDate = new Date(); // empty -> take present
+
+  ngOnInit() {
+
+    var toSet = this.minDate.setFullYear(this.minDate.getFullYear() - 18, this.minDate.getMonth(), this.minDate.getDate());
+    var toSetDate = new Date(toSet);
+    this.minDate = toSetDate;
+
+  }
+
 }
